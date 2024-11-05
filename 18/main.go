5a25518d37bdb3da89f72bb907e06a25eb434bc6
htadmg <agatadominguestec@hotmail.com>
2024-11-05 14:34:35 -0300

    pacotes e modulos

---
+++ 18/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt" 
+	"fundamentos_GO/18/matematica"
+)
+
+func main() {
+	s := matematica.Soma(10, 20)
+	fmt.Println("Resultado:", s)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module fundamentos_GO
+
+go 1.23.2
